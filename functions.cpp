9185588129d3bb0c7db9af72341b0abd1d96d452
Saurabh <saurabh.yelmame19@vit.edu>
2022-03-14 19:15:07 +0530

    main() structure completed

---
+++ functions.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+int createTable(vector<string>parse)
+{
+     cout<<6666666;
+
+    return 0;
+}
+
+int dropTable(vector<string>parse)
+{
+    cout<<"drop";
+    return 0;
+}
+int updateTable(vector<string>parse)
+{
+
+    return 0;
+}
+int selectFromTable(vector<string>parse)
+{
+
+    return 0;
+}
+int deletetFromTable(vector<string>parse)
+{
+
+    return 0;
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,87 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<string>Tokens;
+
+void DisplayTokens()
+{
+    for(string x:Tokens)
+        cout<<x<<",";
+}
+
+void ParseIntoTokens(string token)
+{
+    stringstream space(token);
+    string i;
+
+    while(getline(space,i,' '))
+        Tokens.push_back(i);
+}
+
+void Execute()
+{
+    
+    if(Tokens[0]=="create" && Tokens[1]=="table") 
+    {
+        cout<<"== create table"<<endl;
+    }
+    else if(Tokens[0]=="drop" && Tokens[1]=="table")
+    {
+        cout<<"== drop table"<<endl;
+    }
+    else if(Tokens[0]=="describe")
+    {
+        cout<<"== describe"<<endl;
+    }
+    else if(Tokens[0]=="insert" && Tokens[0]=="into")
+    {
+        cout<<"== insert into"<<endl;
+    }
+    
+    else if(Tokens[0]=="delete" && Tokens[0]=="from")
+    {
+        cout<<"== delete from"<<endl;
+    }
+    else if(Tokens[0]=="update")
+    {
+        cout<<"== update"<<endl;
+    }
+    else if(Tokens[0]=="select")
+    {
+        cout<<"== create table"<<endl;
+    }
+    else if(Tokens[0]=="help" && Tokens[0]=="tables")
+    {
+        cout<<"== help tables"<<endl;
+    }
+    else if(Tokens[0]=="help")
+    {
+        cout<<"== help cmd"<<endl;
+    }
+    else if(Tokens[0]=="quit")
+    {
+        cout<<"== quit"<<endl;
+        exit(0);
+    }
+    
+}
 
 int main()
 {
+    system("cls");
+    string Query;
     
+    while(1)
+    {
+        Tokens.clear(); 
+        cout<<endl<<">> ";
+
+        getline(cin,Query);
+        
+        ParseIntoTokens(Query);
+
+        Execute();  
+    }
+    
+    return 0;
 }
